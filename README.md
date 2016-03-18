3f2ec60e6e9ada075784294d2a269fd05c594933
Jennifer Strater <jennifer.strater@objectpartners.com>
2016-03-18 10:48:33 -0500

    Publish of Github pages from Gradle.

--- README.md
+++
@@ -1,2 +0,0 @@
-# grails-spring-restdocs-example
-Example of adding spring rest docs to sample grails project
--- grails-app/controllers/UrlMappings.groovy
+++
@@ -1,8 +0,0 @@
-class UrlMappings {
-
-    static mappings = {
-        "/"(controller: 'index')
-        "500"(controller: 'InternalServerError')
-        "404"(controller: 'NotFound')
-    }
-}
