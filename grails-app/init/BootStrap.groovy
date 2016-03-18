524247990d003a6b762ec72dbd535f3b9c4f33f9
Jennifer Strater <jennifer.strater@objectpartners.com>
2016-03-17 23:35:37 -0500

    add docs and tests

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,13 @@
+import com.example.Note
+
 class BootStrap {
 
     def init = { servletContext ->
+        environments {
+            test {
+                new Note(title: 'Hello, World!', body: 'Hello from the Integration Test').save()
+            }
+        }
     }
     def destroy = {
     }
